2526791196ce200935bb6f611cac41ed0f6433ba
Blake568 <80367000+Blake568@users.noreply.github.com>
2021-07-08 14:58:50 +1200

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,45 @@
+import time
+time.sleep(3)
+print("Hello and welcome to the quiz, you will be answering multiple questions in the  subject of maths. good luck!") 
+import time
+time.sleep(6)
+print("Make sure to put capital letters at the beginning of a word, you mustn't use    spaces in this quiz")
+import time
+time.sleep(4)
+Number_1 = input("what is 3 x 6?")
+if Number_1 == "18":
+    print("Correct, nice job!")
+else: print("Sorry, wrong answer")
+Number_2 = input("what is 6 index 4 x 3 equal?")
+if Number_2 == "3888":
+    print("You're on fire!")
+else: print("Almost got it")
+if Number_1 == "18" and Number_2 == "3888":
+    print("You're doing so well!")
+else: print("Keep going!")
+Number_3 = input("What is the square root of 36?")
+if Number_3 == "6":
+    print("Correct!")
+else: print(" Nah")
+Number_4 = input("If I had 243 apples and I ate 137, how many apples wuold be left?")
+if Number_4 == "106":
+    print("Good job!")
+else: print("Wrong")
+Number_5 = input("If i owned 64 hats and bought 80 more, how many would I have?")
+if Number_5 == "144":
+    print("Great!")
+else: print("Almost")
+import time
+time.sleep(3)
+print("From here it'll be getting a little harder to answer")
+import time
+time.sleep(4)
+Number_6 = input("A pile of 15 boxes is 3 metres high. What is the depth of each box?")
+if Number_6 == "200mm":
+    print("Good job, that one was tricky")
+else: print("I wouldn't have gotten it either")
+Number_7 = input("When multiplying two negative numbers, what kind of number does one always get for an answer?")
+if Number_7 == "Positive":
+    print("Well done!")
+else: print("Maybe next time")
+print("You have reached the end of this quiz, hope you had fun")
